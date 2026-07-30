db4a9007ce00a04c4ce11a04d4b835657b2c8218
Mehdi <wishtree1248@gmail.com>
2026-07-30 14:32:24 +0330

    Creating CRUD operations for the project

--- core/tasks/models.py
+++ core/tasks/models.py
@@ -6,7 +6,7 @@ class TaskModel (Base):
     __tablename__ = "tasks"
     id = Column(Integer, primary_key = True, autoincrement = True)
     title = Column(String(150), nullable = False)
-    description = Column(String(500), default = True)
+    description = Column(String(500), nullable = True)
     is_completed = Column(Boolean, default = False)
     created_at = Column(DateTime(), server_default = func.now())
     updated_at = Column(DateTime(), server_default = func.now(),
