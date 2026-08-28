aafbffc5afadb0a53b48656e14ac277ca35c9fb5
Mehdi <wishtree1248@gmail.com>
2026-08-28 11:40:06 +0330

    Creating tasks for loadtest

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -1,4 +1,26 @@
 from locust import HttpUser, task, between
 
 class QuickStartUser(HttpUser):
-    pass
+    between(1,2)
+
+    def on_start(self):
+        response = self.client.post("/users/login",
+                         json = {"user_name": "eli", "password": "1248"})
+        access_token = response.json()["access_token"]
+        self.client.headers = {"Authorization": f"Bearer {access_token}"}
+
+    @task
+    def initial_task(self):
+        self.client.get("/initiate-task")
+
+    @task
+    def not_found(self):
+        self.client.get("/not-found")
+
+    @task
+    def tasks_list(self):
+        self.client.get("/tasks")
+
+    @task
+    def fetch_weather(self):
+        self.client.get("/fetch-weather")
