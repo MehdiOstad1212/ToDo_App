e6707472d2b7618a280ea12c11358b5d7a63a7cf
Mehdi <wishtree1248@gmail.com>
2026-08-30 12:39:48 +0330

    Working with Beat and doing scheduled tasks

--- core/core/celery_conf.py
+++ core/core/celery_conf.py
@@ -1,12 +1,25 @@
 from celery import Celery
 from core.config import settings
 import time
+import datetime
 
 celery_app = Celery('worker',
                     broker = settings.CELERY_BROKER_URL,
                     backend = settings.CELERY_BACKEND_URL)
 
-celery_app.conf.update(broker_connection_retry_on_startup=True)
+celery_app.conf.update(broker_connection_retry_on_startup = True,
+                       timezone = "UTC",
+                       beat_schedule = {
+                           "print-hello-every-20-seconds": {
+                               "task": "core.celery_conf.print_hello",
+                               "schedule": 20.0
+                           }
+                       })
+
+@celery_app.task
+def print_hello():
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Hello, World! - {now}")
 
 @celery_app.task
 def add_number(x, y):
