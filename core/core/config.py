c9e00e17ebfa8646fe2e3f684f8fc65566b312d2
Mehdi <wishtree1248@gmail.com>
2026-09-03 14:37:14 +0330

    Adding some primary values for some variables

--- core/core/config.py
+++ core/core/config.py
@@ -2,13 +2,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    SQLAlCHEMY_DATABASE_URL: str
+    SQLAlCHEMY_DATABASE_URL: str = "sqlite:///:memory:"
     JWT_SECRET_KEY: str = "test"
     model_config = SettingsConfigDict(env_file=".env")
 
     SENTRY_DSN:str = "https://75b071b6160b780fac49f1bde10183d3@sentry.hamravesh.com/10409"
 
-    REDIS_URL: str
+    REDIS_URL: str = "redis://redis:6379"
 
     MAIL_USERNAME: str = ""
     MAIL_PASSWORD: str = ""
